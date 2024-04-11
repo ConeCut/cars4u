3bd34e47aa2ea2d573ff9a3899b08909da18ce9c
ConeCut <enachescucosmin2016@gmail.com>
2024-04-11 14:44:36 +0200

    minor adjustments to redirect properly

--- website/login/includes/login.inc.php
+++ website/login/includes/login.inc.php
@@ -48,7 +48,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 
         $_SESSION["last_regeneration"] = time();
 
-        header("Location: ../../index.php?loggedIn=true");
+        header("Location: ../../../index.php?loggedIn=true");
 
         $pdo = null;
         $stmt = null;
